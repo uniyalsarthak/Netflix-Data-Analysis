0c9f958948ae6b18dbdf0b82ebb0d84e9adb09ee
Sarthak Uniyal <sarunixi@gmail.com>
2025-11-10 22:17:50 +0530

    Problems

---
+++ netflix_prob_stt.sql
@@ -0,0 +1,17 @@
+-- Problem Statements
+
+1.Count the number of Movies vs TV Shows
+2.Find the most common rating for movies and TV shows
+3.List all movies released in a specific year (e.g., 2020)
+4.Find the top 5 countries with the most content
+5.Identify the longest movie
+6.Find content added in the last 5 years
+7.Find all the movies/TV shows by a specific director (e.g., Ashwiny Iyer Tiwari)
+8.List all TV shows with more than 5 seasons
+9.Count the number of content items in each genre
+10.Find the average number of Indian movies released per year and top 5 years with highest content release
+11.List all movies that are documentaries
+12.Find all content without a director
+13.Find how many movies actor ‘Salman Khan’ appeared in over the last 10 years
+14.Find the top 10 actors with the highest number of Indian movies
+15.Categorize content as Good or Bad based on violent keywords in descriptions
